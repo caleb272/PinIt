650d237b786680cc2d662744cd2d39aab51e14ce
Caleb Martin <martincaleb@gmail.com>
2016-11-30 17:14:36 -0700

    put in the basic homepage

--- client/modules/App/components/CreatePinDropdown/CreatePinDropdown.js
+++ client/modules/App/components/CreatePinDropdown/CreatePinDropdown.js
@@ -3,25 +3,52 @@ import React, { Component } from 'react'
 import styles from './CreatePinDropdown.css'
 
 class CreatePinDropdown extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      'image-field': '',
+      'description-field': ''
+    }
+
+    this.onTextFieldChanged = this.onTextFieldChanged.bind(this)
+  }
+
+
+  onTextFieldChanged(e) {
+    this.setState({ [e.target.id]: e.target.value })
+  }
+
+
   render() {
-    // teal lighten-2
     return (
       <div className={`${styles.dropdown} red lighten-2`}>
-        <h5 className="center">Add Pin</h5>
+        <h5 className={`${styles.left} left`}>Add Pin</h5>
 
         <div className="row">
           <form className="col s12">
             <div className="row">
               <div className="input-field col s12">
-                <input id="image" type="url" />
-                <label htmlFor="image">Image URL</label>
+                <input
+                  id="image-field"
+                  type="url"
+                  onChange={this.onTextFieldChanged}
+                  value={this.state['image-field']}
+                />
+                <label htmlFor="image-field">Image URL</label>
               </div>
             </div>
 
             <div className="row">
               <div className="input-field col s12">
-                <input id="description" type="text" className="" />
-                <label htmlFor="description">Pin Description</label>
+                <input
+                  id="description-field"
+                  type="text"
+                  onChange={this.onTextFieldChanged}
+                  onKeyDown={() => console.log('enter bitch')}
+                  value={this.state['description-field']}
+                />
+                <label htmlFor="description-field">Pin Description</label>
               </div>
             </div>
           </form>
