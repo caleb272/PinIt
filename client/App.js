c221d1f00027ad01edc145ed9cea1c9f27add9a8
Caleb Martin <martincaleb@gmail.com>
2016-12-01 23:25:17 -0700

    removed the initl modules

--- client/App.js
+++ client/App.js
@@ -1,29 +1,26 @@
 /**
  * Root Component
  */
-import React from 'react';
-import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
-import IntlWrapper from './modules/Intl/IntlWrapper';
+import React from 'react'
+import { Provider } from 'react-redux'
+import { Router, browserHistory } from 'react-router'
 
 // Import Routes
-import routes from './routes';
+import routes from './routes'
 
 // Base stylesheet
-require('./main.css');
+require('./main.css')
 
 export default function App(props) {
   return (
     <Provider store={props.store}>
-      <IntlWrapper>
-        <Router history={browserHistory}>
-          {routes}
-        </Router>
-      </IntlWrapper>
+      <Router history={browserHistory}>
+        {routes}
+      </Router>
     </Provider>
   );
 }
 
 App.propTypes = {
   store: React.PropTypes.object.isRequired,
-};
+}
--- client/reducers.js
+++ client/reducers.js
@@ -6,13 +6,11 @@ import { combineReducers } from 'redux'
 // Import Reducers
 import app from './modules/App/AppReducer'
 import posts from './modules/Post/PostReducer'
-import intl from './modules/Intl/IntlReducer'
 import pin from './modules/Pin/PinReducer'
 
 // Combine all reducers into one root reducer
 export default combineReducers({
   app,
   posts,
-  intl,
   pin
 })
